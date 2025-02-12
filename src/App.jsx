78f78fc61c80e1a176493f3a71c59f3ba62ef494
NguyenDucHung <155704343+NguyenDHung@users.noreply.github.com>
2025-02-12 15:41:12 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,7 @@ import "./App.css";
 import AuthProvider from "./context/AuthContext";
 import RenderRoute from "./routes/RenderRoute";
 import { privateRoutes, publicRoutes } from "./routes/Route";
-import AdminLayout from "./components/layouts/AdminLayout";
-import UserManagerment from "./pages/Admin/UserManagerment";
-import CampaignManagerment from "./pages/Admin/CampaignManagerment";
-import PreOrderCampaign from "./pages/Admin/PreOrderCampaign";
-import Voucher from "./pages/Admin/Voucher";
-import Dashboard from "./pages/Admin/Dashboard";
+
 
 function App() {
   return (
