a5f7e91dd59865267a5629748d3563b00eea37ba
NguyenDucHung <155704343+NguyenDHung@users.noreply.github.com>
2025-02-12 15:33:58 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,15 +19,7 @@ function App() {
           {publicRoutes.map((route, index) => RenderRoute(route, index))}
           {privateRoutes.map((route, index) => RenderRoute(route, index, true))}
         </Routes>
-        <Routes>
-          <Route path="/admin" element={<AdminLayout />}>
-            <Route path="usermanagerment" element={<UserManagerment />} />
-            <Route path="campaignmanagerment" element={<CampaignManagerment />} />
-            <Route path="voucher" element={<Voucher />} />
-            <Route path="pre-ordercampaign" element={<PreOrderCampaign />} />
-            <Route path="dashboard" element={<Dashboard />} />
-          </Route>
-        </Routes>
+  
       </Router>
     </AuthProvider>
   );
